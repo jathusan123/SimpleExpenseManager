522f09417b3b70c890039ea6487f648d712a4714
“jathusan123” <jathusan123.15@cse.mrt.ac.lk>
2017-11-19 19:41:33 +0530

    new

---
+++ EmbeddedDemoExpenseManager.java
@@ -0,0 +1,69 @@
+package lk.ac.mrt.cse.dbs.simpleexpensemanager.control;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.control.ExpenseManager;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.control.exception.ExpenseManagerException;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.impl.EmbeddedAccountDAO;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.impl.EmbeddedTransactionDAO;
+
+
+/**
+ * Created by jathusan123 on 11/19/2017.
+ */
+
+
+
+
+public class EmbeddedDemoExpenseManager extends ExpenseManager {
+
+
+    Context context;
+    public EmbeddedDemoExpenseManager(Context ctx)  {
+        context=ctx;
+        try {
+
+            setup();
+        } catch (ExpenseManagerException e) {
+            e.printStackTrace();
+        }
+    }
+    @Override
+
+    public void setup() throws ExpenseManagerException {
+
+        SQLiteDatabase db = context.openOrCreateDatabase("150245C", context.MODE_PRIVATE, null);
+
+        // create the databases.
+        db.execSQL("CREATE TABLE IF NOT EXISTS Account(" +
+                "accountNo VARCHAR PRIMARY KEY," +
+                "bankName VARCHAR," +
+                "accountHolderName VARCHAR," +
+                "balance REAL" +
+                " );");
+
+
+        db.execSQL("CREATE TABLE IF NOT EXISTS Account_Transaction(" +
+                "Transaction_id INTEGER PRIMARY KEY," +
+                "accountNo VARCHAR," +
+                "expenseType INT," +
+                "amount REAL," +
+                "date DATE," +
+                "FOREIGN KEY (accountNo) REFERENCES Account(accountNo)" +
+                ");");
+
+
+
+
+        EmbeddedAccountDAO accountDAO = new EmbeddedAccountDAO(db);
+
+        setAccountsDAO(accountDAO);
+
+        setTransactionsDAO(new EmbeddedTransactionDAO(db));
+
+
+        /*** End ***/
+
+    }
+}
